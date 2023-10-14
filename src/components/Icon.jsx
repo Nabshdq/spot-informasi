0a44712ad5a63cfb8ed4584513f01c3ff4f01184
NaaoiCode <muhnabil7604@gmail.com>
2023-10-14 20:18:14 +0800

    fix: Update Carousel icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,9 +3,9 @@ import logoSpotInformasiInklusiImage from "../../public/logo-spot-informasi-inkl
 
 const Icon = () => {
     return (
-        <div className="absolute z-40 flex justify-between items-center w-full h-full p-10 pl-16 md:flex-col md:items-start md:justify-center md:gap-10 md:pl-20">
-            <Image src={logoSpotInformasiInklusiImage} alt="Spot Informasi Inklusi" className="w-2/12 md:ml-8 lg:ml-20" />
-            <h3 className="text-white font-bold md:w-1/4 text-center md:text-xl lg:text-3xl">Selamat Datang di Spot Informasi Inklusi</h3>
+        <div className="absolute z-40 top-[40%] flex justify-center items-center gap-4 md:flex-col md:items-start md:pl-[9%] md:top-[30%] md:gap-8">
+            <Image src={logoSpotInformasiInklusiImage} alt="Spot Informasi Inklusi" className="w-[13%] md:w-1/3" />
+            <h3 className="text-white font-bold w-1/2 md:w-1/3 text-center md:text-xl lg:text-3xl">Selamat Datang di Spot Informasi Inklusi</h3>
         </div>
     )
 }
