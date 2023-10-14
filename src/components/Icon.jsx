4364c2e3e39d6501f2106946ad40030c7b020e42
NaaoiCode <muhnabil7604@gmail.com>
2023-10-14 20:43:26 +0800

    fix: Change head icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,7 +3,7 @@ import logoSpotInformasiInklusiImage from "../../public/logo-spot-informasi-inkl
 
 const Icon = () => {
     return (
-        <div className="absolute z-40 top-[40%] flex justify-center items-center gap-4 md:flex-col md:items-start md:pl-[9%] md:top-[30%] md:gap-8">
+        <div className="absolute z-40 top-[40%] flex justify-center items-center gap-4 md:flex-col md:items-start md:pl-[8%] md:top-[30%] md:gap-8">
             <Image src={logoSpotInformasiInklusiImage} alt="Spot Informasi Inklusi" className="w-[13%] md:w-1/3" />
             <h3 className="text-white font-bold w-1/2 md:w-1/3 text-center md:text-xl lg:text-3xl">Selamat Datang di Spot Informasi Inklusi</h3>
         </div>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Spot Informasi</title>
+        <link rel="shortcut icon" href="/logo.jpg" />
       </Head>
       <body>
         <Main />
