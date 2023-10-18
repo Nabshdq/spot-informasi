4725c1a0f7150dfb7701d88625d625486ac14c0c
adnanamiruddin <adnan.amiruddin34@gmail.com>
2023-10-18 22:33:47 +0800

    fix: Resize text on carousel

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,14 +3,14 @@ import logoSpotInformasiInklusiImage from "../../public/logo-spot-informasi-inkl
 
 const Icon = () => {
     return (
-        <div className="absolute z-40 top-[40%] flex justify-center items-center gap-8 md:flex-col md:items-start md:pl-[8%] md:top-[30%] md:gap-8">
+        <div className="absolute z-40 top-[40%] flex justify-center items-center gap-2 md:flex-col md:items-start md:pl-[8%] md:top-[30%] md:gap-8">
             <Image
                 src={logoSpotInformasiInklusiImage}
                 alt="Spot Informasi Inklusi"
                 className="w-[13%] md:w-1/4 md:ml-9 glowing-image"
             />
             <h3
-                className="text-white font-bold w-1/2 text-center md:text-xl lg:text-2xl -ml-7 glowing-text"
+                className="text-white font-bold w-1/2 text-center md:text-xl lg:text-2xl md:-ml-7 2xl:-ml-16 glowing-text"
             >
                 Selamat Datang di<br />Spot Informasi Inklusi
             </h3>
