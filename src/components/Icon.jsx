e1cf6bc3d29cf23cd2eab834ead941c268cb312c
adnanamiruddin <adnan.amiruddin34@gmail.com>
2023-10-18 22:23:08 +0800

    fix: Resize icon on carousel

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,16 +3,16 @@ import logoSpotInformasiInklusiImage from "../../public/logo-spot-informasi-inkl
 
 const Icon = () => {
     return (
-        <div className="absolute z-40 top-[40%] flex justify-center items-center gap-4 md:flex-col md:items-start md:pl-[8%] md:top-[30%] md:gap-8">
+        <div className="absolute z-40 top-[40%] flex justify-center items-center gap-8 md:flex-col md:items-start md:pl-[8%] md:top-[30%] md:gap-8">
             <Image
                 src={logoSpotInformasiInklusiImage}
                 alt="Spot Informasi Inklusi"
                 className="w-[13%] md:w-1/4 md:ml-9 glowing-image"
             />
             <h3
-                className="text-white font-bold w-1/2 2xl:w-1/3 text-center md:text-xl lg:text-2xl -ml-7 glowing-text"
+                className="text-white font-bold w-1/2 text-center md:text-xl lg:text-2xl -ml-7 glowing-text"
             >
-                Selamat Datang di Spot Informasi Inklusi
+                Selamat Datang di<br />Spot Informasi Inklusi
             </h3>
         </div>
     );
